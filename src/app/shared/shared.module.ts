f9a63e022d7194bac58088db411cc05f9d84c160
Bikramjit Singh <Bikramjit.singh@honeywell.com>
2019-10-27 01:18:48 +0530

    e

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './Header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './Footer/footer.component';
 
 @NgModule({
   declarations: [ FooterComponent],
