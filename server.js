34474c42ebc8aee9674a019f5e6956685bfe9a83
Bikramjit Singh <Bikramjit.singh@honeywell.com>
2019-10-27 10:25:58 +0530

    h

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/VasuCardiacCare/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 1337);
