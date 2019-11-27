502d85ea2c2feb4c9a3d9a820d28920a169d9030
Bikramjit Singh <Bikramjit.singh@honeywell.com>
2019-11-27 15:32:58 +0530

    gif fixes

--- src/app/modules/Interventional Procedures/components/interventional-procedures/interventional-procedures.component.ts
+++ src/app/modules/Interventional Procedures/components/interventional-procedures/interventional-procedures.component.ts
@@ -18,14 +18,14 @@ export class InterventionalProceduresComponent implements OnInit {
 ];
 
 animationsArray = [
-  {  img: false ,imgsrc : "../../../../../assets/images/OPD consultation.mp4" ,title: "OPD Consultation", description: 'Consult award-winning Cardiologists' , option:'Consult'},
+  {  img: true ,imgsrc : "../../../../../assets/images/consult.gif" ,title: "OPD Consultation", description: 'Consult award-winning Cardiologists' , option:'Consult'},
   { img: true ,imgsrc : "../../../../../assets/images/Master Cardiac care.gif" ,title: "Master Cardiac Check-up", description: 'Consult award-winning Cardiologists' , option:'Consult'},
-  {  img: false , imgsrc : "../../../../../assets/images/2D echo final 1.mp4" ,title: "2D ECHO", description: 'Consult award-winning Cardiologists' , option:'Consult'},
+  {  img: true , imgsrc : "../../../../../assets/images/2D echo final.gif" ,title: "2D ECHO", description: 'Consult award-winning Cardiologists' , option:'Consult'},
 ]
 animationsArray2 = [
   { img: true ,imgsrc : "../../../../../assets/images/TMH-final-10.gif" ,title: "TMT Treadmill", description: 'Consult award-winning Cardiologists' , option:'Consult'},
   {  img: true ,imgsrc : "../../../../../assets/images/x ray_4.gif" ,title: "X-RAY", description: 'Consult award-winning Cardiologists' , option:'Consult'},
-  {  img: false ,imgsrc : "../../../../../assets/images/ECG final.mp4" ,title: "ECG", description: 'Prevent health issues with early diagnosis' , option:'Consult'},
+  {  img: true ,imgsrc : "../../../../../assets/images/ECG.gif" ,title: "ECG", description: 'Prevent health issues with early diagnosis' , option:'Consult'},
  
 ];
 
--- src/app/modules/Interventional Procedures/components/makeappointment/makeappointment.component.ts
+++ src/app/modules/Interventional Procedures/components/makeappointment/makeappointment.component.ts
@@ -11,8 +11,8 @@ export class MakeappointmentComponent implements OnInit {
   LoadAnimationCArd = 'animation-card';
   BookingArray1 = [
     { img: true , imgsrc : "../../../../../assets/images/TMH-final-10.gif" ,title: "Online appointment", description: 'Measure your blood circulation' , option:'Book Now'},
-    { img: true , imgsrc : "../../../../../assets/images/Group 4337.svg" ,title: "Emergency case", description: 'Prevent health issues with early diagnosis' , option:'Call Us'},
-    { img: false , imgsrc : "../../../../../assets/images/OPD consultation.mp4" ,title: "Our doctors", description: 'Prevent health issues with early diagnosis' , option:'Doctors'},
+    { img: true , imgsrc : "../../../../../assets/images/x ray_4.gif" ,title: "Emergency case", description: 'Prevent health issues with early diagnosis' , option:'Call Us'},
+    { img: true , imgsrc : "../../../../../assets/images/consult.gif" ,title: "Our doctors", description: 'Prevent health issues with early diagnosis' , option:'Doctors'},
   ];
   constructor() { }
 
