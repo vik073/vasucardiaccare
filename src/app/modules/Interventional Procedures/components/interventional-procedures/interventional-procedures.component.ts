8c37ad7285e17d28400fdc1194422e8ba5b78bb3
Ajit Joshi <ajit@kurage.co.in>
2019-12-06 06:50:42 +0530

    new changes

--- src/app/modules/Interventional Procedures/components/interventional-procedures/interventional-procedures.component.ts
+++ src/app/modules/Interventional Procedures/components/interventional-procedures/interventional-procedures.component.ts
@@ -22,7 +22,8 @@ animationsArray = [
   {  img: true ,imgsrc : "../../../../../assets/images/consult.gif" ,title: "OPD Consultation", description: 'Consult award-winning Cardiologists' , option:'Consult'},
   { img: true ,imgsrc : "../../../../../assets/images/Master Cardiac care.gif" ,title: "Master Cardiac Check-up", description: 'Consult award-winning Cardiologists' , option:'Consult'},
   {  img: true , imgsrc : "../../../../../assets/images/2D echo final.gif" ,title: "2D ECHO", description: 'Consult award-winning Cardiologists' , option:'Consult'},
-
+]
+animationsArray2 =[ 
   { img: true ,imgsrc : "../../../../../assets/images/TMH-final-10.gif" ,title: "TMT Treadmill", description: 'Consult award-winning Cardiologists' , option:'Consult'},
   {  img: true ,imgsrc : "../../../../../assets/images/x ray_4.gif" ,title: "X-RAY", description: 'Consult award-winning Cardiologists' , option:'Consult'},
   {  img: true ,imgsrc : "../../../../../assets/images/ECG.gif" ,title: "ECG", description: 'Prevent health issues with early diagnosis' , option:'Consult'},
