43e5f39b91c67a9278e0767301475a736fbb8770
Bikramjit Singh <Bikramjit.singh@honeywell.com>
2019-12-10 00:28:41 +0530

    changes done

--- src/app/modules/Interventional Procedures/components/makeappointment/makeappointment.component.ts
+++ src/app/modules/Interventional Procedures/components/makeappointment/makeappointment.component.ts
@@ -11,9 +11,9 @@ export class MakeappointmentComponent implements OnInit {
 
   LoadAnimationCArd = 'animation-card';
   BookingArray1 = [
-    { img: true , imgsrc : "../../../../../assets/images/TMH-final-10.gif" ,title: "Online appointment", description: 'Measure your blood circulation' , option:'Book Now'},
-    { img: true , imgsrc : "../../../../../assets/images/x ray_4.gif" ,title: "Emergency case", description: 'Prevent health issues with early diagnosis' , option:'Call Us'},
-    { img: true , imgsrc : "../../../../../assets/images/consult.gif" ,title: "Our doctors", description: 'Prevent health issues with early diagnosis' , option:'Doctors'},
+    { img: true , imgsrc : "../../../../../assets/images/Group 4512.svg" ,title: "Online appointment", description: 'Schedule a Consultation' , option:'Book Now'},
+    { img: true , imgsrc : "../../../../../assets/images/Group 4508.svg" ,title: "Emergency case", description: 'Ring our medical helpline' , option:'Call Us'},
+    { img: true , imgsrc : "../../../../../assets/images/Group 4515.svg" ,title: "Our doctors", description: 'Know Your Doctors' , option:'Doctors'},
   ];
   mobileview: unknown;
   constructor(private service: ViewfinderService) { }
