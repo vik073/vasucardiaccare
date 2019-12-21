4ddf772f246c27dfb452871fedeeeb9ab18a2638
rajesh <kurage-hunk@Rajeshs-iMac.local>
2019-12-21 22:34:08 +0530

    final changes

--- src/app/modules/homepage/home/home.component.ts
+++ src/app/modules/homepage/home/home.component.ts
@@ -18,20 +18,20 @@ export class HomeComponent implements OnInit {
 ngOnInit(){
   
   
-  this.route.queryParams
-      .subscribe(params => {
-        console.log(params); // {order: "popular"}
-        if(params['view']!=null){ 
-        let el = document.getElementById(params['view']);
-            el.scrollIntoView();
+  // this.route.queryParams
+  //     .subscribe(params => {
+  //       console.log(params); // {order: "popular"}
+  //       if(params['view']!=null){ 
+  //       let el = document.getElementById(params['view']);
+  //           el.scrollIntoView();
             
-        }
-        else{
+  //       }
+  //       else{
           
-          let el = document.getElementById(params['canvas-container']);
-            el.scrollIntoView();
-        }
-      });
+  //         let el = document.getElementById(params['canvas-container']);
+  //           el.scrollIntoView();
+  //       }
+  //     });
   this.service.checkWidth()
     this.service.isMobile.subscribe( x =>{
 
